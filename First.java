4492d541cec240d944451d800ac61a484bbb1056
EgzonOsmani <oegzon73@gmail.com>
2023-04-09 16:15:30 -0700

    new

--- First.java
+++ First.java
@@ -1,3 +1,13 @@
-public class First{
-    System.out.println("egzon");
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello world");
+        int name = Integer.valueOf(scanner.nextLine());
+        if (name < 100){
+            System.out.println("I'm the best");
+        }
+    }
+
 }
